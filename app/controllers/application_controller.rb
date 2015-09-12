9f045d96d68ed9b4484fa3b90faa219caba2ee53
Niclas Nilsson <niclas@Niclass-MacBook-Pro.local>
2015-09-12 17:38:10 +0200

    Handle CORS for local development

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
+
+  after_action :set_access_control_headers!
+
+  def set_access_control_headers!
+    headers['Access-Control-Allow-Origin'] = "http://localhost:9000"
+    headers['Access-Control-Allow-Credentials'] = "true"
+    headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
+  end
 end
