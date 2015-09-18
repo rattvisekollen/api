84bd73975d40ea9ecb485b62df92c901b0047dfc
Niclas Nilsson <niclas@Niclass-MacBook-Pro.local>
2015-09-18 16:06:44 +0200

    Allow basic HTTP request types

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
   def set_access_control_headers!
     headers['Access-Control-Allow-Origin'] = request.headers["origin"]
     headers['Access-Control-Allow-Credentials'] = "true"
-    headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
+    headers['Access-Control-Request-Method'] = %w{GET POST PUT PATCH DELETE OPTIONS}.join(",")
   end
 end
