cec3ebb072d06893b7202c3ea700982574009e18
Niclas Nilsson <niclas@Niclass-MacBook-Pro.local>
2015-09-18 16:06:37 +0200

    Allow all hosts origins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   after_action :set_access_control_headers!
 
   def set_access_control_headers!
-    headers['Access-Control-Allow-Origin'] = "http://localhost:9000"
+    headers['Access-Control-Allow-Origin'] = request.headers["origin"]
     headers['Access-Control-Allow-Credentials'] = "true"
     headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
   end
